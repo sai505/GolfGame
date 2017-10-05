3b4d6354263bc6db7de8a5065f2b7a19919af843
sai505 <seungjun88@gmail.com>
2017-10-05 03:33:46 -0400

    Rule

--- GolfGame/GolfGame/main.cpp
+++ GolfGame/GolfGame/main.cpp
@@ -1,3 +1,18 @@
+//OOP Class. Seung Jun Lee. 2017
+//four-card Golf Game Rule
+//1.The dealer deal four card (layout card) to each player one at a time
+//2.All of undealt card facedown. Drawn card should be placed different stock (faceup).
+//3.Each turn, Each player can see their two cards (whichever) only once and always facedown.
+//4.The other two unseen cards can be looked when discard or until end of game (not each turn).
+//5.The play goes clockwise, but here i will only implement two players
+//6.Each player can draw the card (either from undelat stock or discarded stock) and replace it
+//with one from own's layout card. Or pass the turn.
+//7.The card drawn(faceup) from the endealt stock can be discarded if player donot want to replace 
+//Scoring:
+//A:1, 2~9, J=Q=10, K=0, Joker1=20, Joker2=-20
+//Winning: 
+//The player who has lowest cummulative score win after 10 rounds
+
 class Player
 {
 	Card cards[4];
@@ -18,8 +33,8 @@ class Game
 private:
 	Player* players;
 };
-sdfsfd
-sdfsfassdf sdfkl 
+
+
 using Discard = std::stack<Card>;
 using Deck = std::vector<Card>;
 
